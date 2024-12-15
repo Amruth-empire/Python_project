072ef2b2272a545956cd14e27b27c846c0cc6c4e
Amruth -empire <msamruth74@gmail.com>
2024-12-15 17:25:08 +0530

    Created a new project called scanner

---
+++ QRCode.py
@@ -0,0 +1,10 @@
+import qrcode as qr 
+upi_id=input("Enter your UPI ID = ")
+
+#upi://pay?pa=UPI_ID&pn=NAME&am=Amount%cu=CURRENCY&tn=MESSAGE  &am=100
+
+scanner_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&cu=INR&tn=Thank%20you'
+
+scanner=qr.make(scanner_url)
+scanner.save("Google_pay_scanner.png")
+scanner.show()
