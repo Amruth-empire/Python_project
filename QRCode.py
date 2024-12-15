85309cc3ebf01fdb36a972a651c009d07377f720
Amruth -empire <msamruth74@gmail.com>
2024-12-15 17:38:01 +0530

    deleted a image

--- QRCode.py
+++ QRCode.py
@@ -6,5 +6,5 @@ upi_id=input("Enter your UPI ID = ")
 scanner_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&cu=INR&tn=Thank%20you'
 
 scanner=qr.make(scanner_url)
-scanner.save("Google_pay_scanner.png")
+# scanner.save("Google_pay_scanner.png")
 scanner.show()
